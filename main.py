a855c8e9fdd728c9bb864ec1f7f48ce900077e6c
rebeccayaelraccah <rebecca_raccah@hotmail.com>
2021-02-23 19:46:40 +0200

    Organized between epoch of real and simulated

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from torch.utils.data import Dataset
 from scipy.io import loadmat
 import os
 
-REAL_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Real Database")
+REAL_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Real Database") #TODO sistemare grandezza
 SIMULATED_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Merged Simulated Database")
 
-BATCH_SIZE = 16
+BATCH_SIZE = 16 #TODO vedere bene i parametri
 epochs = 20
 learning_rate = 1e-3
 delta = 1e-2
@@ -35,18 +35,18 @@ class ResNet(nn.Module):
             f_out, one_before_last_f = self.Fdecoder(f)
             return m_out,one_before_last_m,f_out,one_before_last_f
 
-# class RealDataset(Dataset):
-#     def __init__(self, real_dir):
-#         self.real_dir = real_dir
-#         self.real_signals = os.listdir(real_dir)
-#
-#     def __len__(self):
-#         return len(self.real_signals)
-#
-#     def __getitem__(self, idx):
-#         path_signal = os.path.join(self.real_dir, self.real_signals[idx])
-#         signal = loadmat(path_signal)['data']
-#         return signal
+class RealDataset(Dataset):
+    def __init__(self, real_dir):
+        self.real_dir = real_dir
+        self.real_signals = os.listdir(real_dir)
+
+    def __len__(self):
+        return len(self.real_signals)
+
+    def __getitem__(self, idx):
+        path_signal = os.path.join(self.real_dir, self.real_signals[idx])
+        signal = torch.from_numpy(loadmat(path_signal)['data'])
+        return signal
 
 class SimulatedDataset(Dataset):
     def __init__(self, simulated_dir,list):
@@ -56,21 +56,11 @@ class SimulatedDataset(Dataset):
     def __len__(self):
         return len(self.simulated_signals)
 
-    # def get_m(self,idx):
-    #     path_mecg = os.path.join(self.simulated_dir, self.simulated_signals[idx][1])
-    #     mecg = torch.from_numpy(loadmat(path_mecg)['data'])
-    #     return mecg
-    #
-    # def get_f(self,idx):
-    #     path_fecg = os.path.join(self.simulated_dir, self.simulated_signals[idx][2])
-    #     fecg = torch.from_numpy(loadmat(path_fecg)['data'])
-    #     return fecg
-
     def __getitem__(self, idx):
         path_mix = os.path.join(self.simulated_dir, self.simulated_signals[idx][0])
         path_mecg = os.path.join(self.simulated_dir, self.simulated_signals[idx][1])
         path_fecg = os.path.join(self.simulated_dir, self.simulated_signals[idx][2])
-        mix =  torch.from_numpy(loadmat(path_mix)['data'])
+        mix =  torch.from_numpy(loadmat(path_mix)['data']) #TODO cambiare il cast
         mecg =  torch.from_numpy(loadmat(path_mecg)['data'])
         fecg =  torch.from_numpy(loadmat(path_fecg)['data'])
         return mix,mecg,fecg
@@ -78,19 +68,19 @@ class SimulatedDataset(Dataset):
 def main():
 
     list_simulated = simulated_database_list(SIMULATED_DATASET)
-    #real_dataset = RealDataset(REAL_DATASET)
+    real_dataset = RealDataset(REAL_DATASET)
     simulated_dataset = SimulatedDataset(SIMULATED_DATASET,list_simulated)
 
-    # train_size_real = int(0.8 * len(real_dataset))
-    # test_size_real = len(real_dataset) - train_size_real
-    # train_dataset_real, test_dataset_real = torch.utils.data.random_split(real_dataset, [train_size_real, test_size_real])
+    train_size_real = int(0.8 * len(real_dataset))
+    test_size_real = len(real_dataset) - train_size_real
+    train_dataset_real, test_dataset_real = torch.utils.data.random_split(real_dataset, [train_size_real, test_size_real])
 
     train_size_sim = int(0.8 * len(simulated_dataset))
     test_size_sim = len(simulated_dataset) - train_size_sim
     train_dataset_sim, test_dataset_sim = torch.utils.data.random_split(simulated_dataset, [train_size_sim, test_size_sim])
 
-    # train_data_loader_real = data.DataLoader(train_dataset_real, batch_size=BATCH_SIZE, shuffle=True)
-    # test_data_loader_real = data.DataLoader(test_dataset_real, batch_size=BATCH_SIZE, shuffle=True)
+    train_data_loader_real = data.DataLoader(train_dataset_real, batch_size=BATCH_SIZE, shuffle=True)
+    test_data_loader_real = data.DataLoader(test_dataset_real, batch_size=BATCH_SIZE, shuffle=True)
 
     train_data_loader_sim = data.DataLoader(train_dataset_sim, batch_size=BATCH_SIZE, shuffle=True)
     test_data_loader_sim = data.DataLoader(test_dataset_sim, batch_size=BATCH_SIZE, shuffle=True)
@@ -109,24 +99,39 @@ def main():
         total_loss_epoch = 0
         total_loss_m = 0
         total_loss_f = 0
+        total_loss_ecg = 0
         total_loss_cent = 0
         total_loss_hinge = 0
 
-        for i, batch_features in enumerate(train_data_loader_sim):
-            batch_for_model = batch_features[0].to(device)
-            batch_for_m =  batch_features[1].to(device)
-            batch_for_f = batch_features[2].to(device)
-            # batch_features = batch_features.to(device)
+        real_epoch = ((epoch % 5) == 0)
+
+        if (real_epoch):
+            data_loader = train_data_loader_real
+        else:
+            data_loader = train_data_loader_sim
+
+        for i, batch_features in enumerate(data_loader):
+            if (real_epoch):
+                batch_for_model = batch_features.to(device)
+            else:
+                batch_for_model = batch_features[0].to(device)
+                batch_for_m =  batch_features[1].to(device)
+                batch_for_f = batch_features[2].to(device)
+
             optimizer_model.zero_grad()
             optimizer_centloss.zero_grad()
 
             outputs_m,one_before_last_m,outputs_f,one_before_last_f = resnet_model(batch_for_model).to(device)
 
-            #COST(M,M^)
-            train_loss_mecg = criterion(outputs_m, batch_for_m)
+            if(not real_epoch):
+                #COST(M,M^)
+                train_loss_mecg = criterion(batch_for_m,outputs_m)
+                #COST(F,F^)
+                train_loss_fecg = criterion(batch_for_f,outputs_f)
+            else:
+                outputs_m = torch.add(outputs_m,outputs_f)
+                train_loss_ecg = criterion(batch_for_model,outputs_m)
 
-            #COST(F,F^)
-            train_loss_fecg = criterion(outputs_f, batch_for_f)
 
             #Center loss(one before last decoder M, one before last decoder F)
             flatten_m,flatten_f = torch.flatten(one_before_last_m,start_dim=1), torch.flatten(one_before_last_f,start_dim=1) #TODO check
@@ -138,27 +143,41 @@ def main():
             #Clustering loss(one before last decoder M, one before last decoder F)
             hinge_loss = hinge_loss(one_before_last_m, one_before_last_f,delta)
 
-            total_loss = train_loss_mecg + train_loss_fecg + loss_cent + hinge_loss
+            if(not real_epoch):
+                total_loss = train_loss_mecg + train_loss_fecg + loss_cent + hinge_loss
+            else:
+                total_loss = train_loss_ecg + loss_cent + hinge_loss
             total_loss.backward()
             optimizer_model.step()
             optimizer_centloss.step()
 
-            total_loss_m += train_loss_mecg.item()
-            total_loss_f += train_loss_fecg.item()
+            if(not real_epoch):
+                total_loss_m += train_loss_mecg.item()
+                total_loss_f += train_loss_fecg.item()
+            else:
+                total_loss_ecg += train_loss_ecg.item()
             total_loss_cent += loss_cent.item()
             total_loss_hinge += hinge_loss.item()
-            total_loss_epoch += total_loss
+            total_loss_epoch += total_loss #TODO non devo dividere anche questo per len(data_loader) ?
             del batch_features
             torch.cuda.empty_cache()
 
         # compute the epoch training loss
-        total_loss_m = total_loss_m / (len(train_data_loader_sim))
-        total_loss_f = total_loss_f / (len(train_data_loader_sim))
-        total_loss_cent = total_loss_cent / (len(train_data_loader_sim))
-        total_loss_hinge = total_loss_hinge / (len(train_data_loader_sim))
+        if(not real_epoch):
+            total_loss_m = total_loss_m / (len(data_loader))
+            total_loss_f = total_loss_f / (len(data_loader))
+        else:
+            total_loss_ecg = total_loss_ecg / (len(data_loader))
+        total_loss_cent = total_loss_cent / (len(data_loader))
+        total_loss_hinge = total_loss_hinge / (len(data_loader))
 
         # display the epoch training loss
-        print("epoch : {}/{}, total_loss = {:.8f}, loss_mecg = {:.8f}, loss_fecg = {:.8f}, loss_cent = {:.8f}, loss_hinge = {:.8f}".format(epoch + 1, epochs, total_loss_epoch, total_loss_m, total_loss_f, total_loss_cent, total_loss_hinge))
+        if(not real_epoch):
+            print("epoch S : {}/{}, total_loss = {:.8f}, loss_mecg = {:.8f}, loss_fecg = {:.8f}, loss_cent = {:.8f}, loss_hinge = {:.8f}".format(epoch + 1, epochs, total_loss_epoch, total_loss_m, total_loss_f, total_loss_cent, total_loss_hinge))
+        else:
+            print(
+                "epoch R : {}/{}, total_loss = {:.8f}, loss_ecg = {:.8f}, loss_cent = {:.8f}, loss_hinge = {:.8f}".format(
+                    epoch + 1, epochs, total_loss_epoch, total_loss_ecg, total_loss_cent, total_loss_hinge))
 
     del resnet_model
     del simulated_dataset
