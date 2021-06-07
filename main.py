0692be04538467811ad1f2c235affd8fec0c7b5d
rebecca <rebecca_raccah@hotmail.com>
2021-06-07 16:23:03 +0300

    real with model load

--- main.py
+++ main.py
@@ -16,15 +16,8 @@ import wfdb
 from EarlyStopping import *
 import scipy.fftpack as function
 from SignalPreprocessing.data_preprocess_function import *
-ECG_OUTPUTS_TEST_REAL = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                            "ECGOutputsTestReal")
-if not os.path.exists(ECG_OUTPUTS_TEST_REAL):
-    os.mkdir(ECG_OUTPUTS_TEST_REAL)
 
-
-SIMULATED_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)), "simulated_windows_noise")
-#SIMULATED_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)), "SimulatedDatabase")
-REAL_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)), "NewReal")
+SIMULATED_DATASET = os.path.join(os.path.dirname(os.path.realpath(__file__)), "SimulatedDatabase")
 
 LOSSES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Losses")
 if not os.path.exists(LOSSES):
@@ -38,44 +31,10 @@ BATCH_SIZE = 32
 epochs = 20
 learning_rate = 1e-3
 
-def inference(filename, test_data_loader_real):
-    resnet_model = ResNet(1)
-    resnet_model.load_state_dict(torch.load('./best_model'))
-    resnet_model.eval()
-
-    resnet_model.cuda()
-
-    criterion = nn.L1Loss().cuda()
-
-    test_loss_ecg = 0
-    test_corr_ecg = 0
-    with torch.no_grad():
-        for i, batch_features in enumerate(test_data_loader_real):
-            batch_for_model_test = Variable(1000. * batch_features.float().cuda())
-            outputs_m_test_real, _, outputs_f_test_real, _ = resnet_model(batch_for_model_test)
-            test_loss_ecg += criterion(outputs_m_test_real + outputs_f_test_real, batch_for_model_test)
-
-            for j, elem in enumerate(outputs_f_test_real):
-                test_corr_ecg += np.corrcoef((outputs_m_test_real[j] + outputs_f_test_real[j]).cpu().detach().numpy(), batch_for_model_test.cpu().detach().numpy()[j])[0][1]
-                path = os.path.join(ECG_OUTPUTS_TEST_REAL, "label_ecg" + str(j) + str(i))
-                np.save(path, batch_features[j].cpu().detach().numpy())
-                path = os.path.join(ECG_OUTPUTS_TEST_REAL, "ecg" + str(j) + str(i))
-                np.save(path, (outputs_m_test_real[j] + outputs_f_test_real[j]).cpu().detach().numpy() / 1000.)
-                path = os.path.join(ECG_OUTPUTS_TEST_REAL, "mecg" + str(j) + str(i))
-                np.save(path, (outputs_m_test_real[j]).cpu().detach().numpy() / 1000.)
-                path = os.path.join(ECG_OUTPUTS_TEST_REAL, "fecg" + str(j) + str(i))
-                np.save(path, (outputs_f_test_real[j]).cpu().detach().numpy() / 1000.)
-
-        test_loss_ecg /= len(test_data_loader_real.dataset)
-        test_corr_ecg /= len(test_data_loader_real.dataset)
-        print('L1: ' + str(test_loss_ecg.item()))
-        print('Corr: ' + str(test_corr_ecg))
-
-
 def main():
 
     pl.seed_everything(1234)
-    list_simulated = simulated_database_list(SIMULATED_DATASET)[:10]#[:122740]
+    list_simulated = simulated_database_list(SIMULATED_DATASET)[:10]
 
     #list_simulated = remove_nan_signals(list_simulated)
 
@@ -88,8 +47,6 @@ def main():
     train_dataset_sim, val_dataset_sim, test_dataset_sim = torch.utils.data.random_split(simulated_dataset, [train_size_sim, val_size_sim,test_size_sim])
 
     train_data_loader_sim = data.DataLoader(train_dataset_sim, batch_size=BATCH_SIZE, shuffle=True, num_workers=12)
-    val_data_loader_sim = data.DataLoader(val_dataset_sim, batch_size=BATCH_SIZE, shuffle=False)
-    test_data_loader_sim = data.DataLoader(test_dataset_sim, batch_size=BATCH_SIZE, shuffle=False)
 
     #  use gpu if available
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
@@ -98,7 +55,6 @@ def main():
     resnet_model = ResNet(1).cuda()
     best_model_accuracy = - math.inf
     val_loss = 0
-    early_stopping = EarlyStopping(delta_min=0.01, patience=6, verbose=True)
     criterion = nn.L1Loss().cuda()
     criterion_cent = CenterLoss(num_classes=2, feat_dim=512*64, use_gpu=device)
     params = list(resnet_model.parameters()) + list(criterion_cent.parameters())
@@ -129,7 +85,7 @@ def main():
               train_loss_average_list)
         #Evaluation
         resnet_model.eval()
-        best_model_accuracy, val_loss = val(val_data_loader_sim,
+        best_model_accuracy, val_loss = val(train_data_loader_sim,
                                     resnet_model,
                                     criterion,
                                     epoch,
@@ -139,13 +95,8 @@ def main():
                                     validation_loss_average_list,
                                     validation_corr_m_list,
                                     validation_corr_f_list,
-                                    best_model_accuracy,
-                                    criterion_cent)
+                                    best_model_accuracy)
         scheduler.step()
-        early_stopping(val_loss, resnet_model)
-        if early_stopping.early_stop:
-            print('Early stopping')
-            break
 
     #Saving graphs training
     path_losses = os.path.join(LOSSES, "TL1M")
@@ -200,277 +151,3 @@ def main():
 if __name__=="__main__":
 
     main()
-    """real_dataset = dataloader.RealDataset(REAL_DATASET)
-    test_data_loader_real = data.DataLoader(real_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    inference(network_save_folder_orig,test_data_loader_real)
-
-    for filename in os.listdir(ECG_OUTPUTS_TEST_REAL):  # present the fecg outputs
-        if "ecg" in filename:
-            path = os.path.join(ECG_OUTPUTS_TEST_REAL, filename)
-            number_file = filename.index("g") + 1
-            end_path = filename[number_file:]
-            path_label = os.path.join(ECG_OUTPUTS_TEST_REAL, "label_ecg" + end_path)
-            mecg_label = os.path.join(ECG_OUTPUTS_TEST_REAL, "mecg" + end_path)
-            fecg_label = os.path.join(ECG_OUTPUTS_TEST_REAL, "fecg" + end_path)
-            fig, (ax1, ax2,ax3,ax4) = plt.subplots(4, 1)
-            ax1.plot(np.load(path)[0])
-            ax1.set_ylabel("ECG")
-            ax2.plot(np.load(path_label)[0])
-            ax2.set_ylabel("LABEL ECG")
-            ax3.plot(np.load(mecg_label)[0])
-            ax3.set_ylabel("MECG")
-            ax4.plot(np.load(fecg_label)[0])
-            ax4.set_ylabel("FECG")
-            plt.show()
-            plt.close()
-           
-
-
-    BAR_LIST = os.path.join(os.path.dirname(os.path.realpath(__file__)), "BarListTest")
-
-
-    #BAR REPRESENTATION
-    ind = np.arange(4)
-    x_labels = ['NONE', 'MA', 'MA+EM', 'MA+EM+BW']
-    results = np.load(os.path.join(BAR_LIST,"list_bar_bad_example_noisetype.npy"))
-    sum = np.sum(np.matrix(results))
-    plt.bar(ind,results)
-    plt.title('Failing signals according to noise type. Total: {}'.format(sum))
-    plt.xticks(ind,('NONE', 'MA', 'MA+EM', 'MA+EM+BW'))
-    plt.show()
-    plt.close()
-
-
-    ind = np.arange(4)
-    x_labels = ['NONE', 'MA', 'MA+EM', 'MA+EM+BW']
-    results = np.load(os.path.join(BAR_LIST,"list_bar_good_example_noisetype.npy"))
-    sum = np.sum(np.matrix(results))
-    plt.bar(ind,results)
-    plt.title('Successful signals according to noise type. Total: {}'.format(sum))
-    plt.xticks(ind,('NONE', 'MA', 'MA+EM', 'MA+EM+BW'))
-    plt.show()
-    plt.close()
-
-    ind = np.arange(5)
-    x_labels = ['00', '03', '06', '09', '12']
-    results = np.load(os.path.join(BAR_LIST,"list_bar_bad_example_snr.npy"))
-    sum = np.sum(np.matrix(results))
-    plt.bar(ind,results)
-    plt.title('Failing signals according to SNR [dB]. Total: {}'.format(sum))
-    plt.xticks(ind,('00', '03', '06', '09', '12'))
-    plt.show()
-    plt.close()
-
-    ind = np.arange(5)
-    x_labels = ['00', '03', '06', '09', '12']
-    results = np.load(os.path.join(BAR_LIST, "list_bar_good_example_snr.npy"))
-    sum = np.sum(np.matrix(results))
-    plt.bar(ind, results)
-    plt.title('Successful signals according to SNR [dB]. Total: {}'.format(sum))
-    plt.xticks(ind, ('00', '03', '06', '09', '12'))
-    plt.show()
-    plt.close()
-
-
-    X = np.arange(7)
-    data = np.load(os.path.join(BAR_LIST, "list_bar_bad_example_snrcase.npy"))
-    print(np.sum(data, axis=0))
-    sum = np.sum(np.matrix(data))
-    a = plt.bar(X, data[0], color='b', width=0.1)
-    b = plt.bar(X + 0.1 , data[1], color='g', width=0.1)
-    c = plt.bar(X + 0.2, data[2], color='r', width=0.1)
-    d = plt.bar(X + 0.3, data[3], color='c', width=0.1)
-    e = plt.bar(X + 0.4, data[4], color='y', width=0.1)
-    plt.legend((a, b, c, d, e), ('00', '03', '06', '09', '12'))
-    plt.xticks(X, ('CO', 'C1', 'C2', 'C3','C5', 'C4', 'BASELINE'))
-    plt.title('Failing signals according to physiological case and SNR [dB]. Total: {}'.format(sum))
-    plt.show()
-    plt.close()
-   
-    X = np.arange(7)
-    data = np.load(os.path.join(BAR_LIST, "list_bar_good_example_snrcase.npy"))
-    print(np.sum(data,axis=0))
-    sum = np.sum(np.matrix(data))
-    a = plt.bar(X, data[0], color='b', width=0.1)
-    b = plt.bar(X + 0.1, data[1], color='g', width=0.1)
-    c = plt.bar(X + 0.2, data[2], color='r', width=0.1)
-    d = plt.bar(X + 0.3, data[3], color='c', width=0.1)
-    e = plt.bar(X + 0.4, data[4], color='y', width=0.1)
-    plt.legend((a, b, c, d, e), ('00', '03', '06', '09', '12'))
-    plt.xticks(X, ('CO', 'C1', 'C2', 'C3', 'C4', 'C5', 'BASELINE'))
-    plt.title('Successful signals according to physiological case and SNR [dB]. Total: {}'.format(sum))
-    plt.show()
-    plt.close()
-    
-    
-    #DROPOUT1
-
-    LOSSESBASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Losses" + str(127740))
-    LOSSESLDROP = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Losses")
-    path_losses = os.path.join(LOSSESBASE, "VL1M.npy")
-    m1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESBASE, "VL1F.npy")
-    f1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESBASE, "VL1Avg.npy")
-    a1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "VL1M.npy")
-    m2 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "VL1F.npy")
-    f2 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "VL1Avg.npy")
-    a2 = np.load(path_losses)[:20]
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.plot(m1, label="noDROP")
-    ax1.plot(m2, label="DROP")
-    ax1.set_ylabel("L1 M")
-    ax1.set_xlabel("Epoch")
-    ax2.plot(f1, label="noDROP")
-    ax2.plot(f2, label="DROP")
-    ax2.set_ylabel("L1 F")
-    ax2.set_xlabel("Epoch")
-    ax3.plot(a1, label="noDROP")
-    ax3.plot(a2, label="DROP")
-    ax3.set_ylabel("L1 Avg")
-    ax3.set_xlabel("Epoch")
-    ax1.legend()
-    ax2.legend()
-    ax3.legend()
-    plt.show()
-    plt.close()
-    path_losses = os.path.join(LOSSESBASE, "TL1M.npy")
-    m1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESBASE, "TL1F.npy")
-    f1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESBASE, "TL1Avg.npy")
-    a1 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "TL1M.npy")
-    m2 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "TL1F.npy")
-    f2 = np.load(path_losses)[:20]
-    path_losses = os.path.join(LOSSESLDROP, "TL1Avg.npy")
-    a2 = np.load(path_losses)[:20]
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.plot(m1, label="noDROP")
-    ax1.plot(m2, label="DROP")
-    ax1.set_ylabel("L1 M")
-    ax1.set_xlabel("Epoch")
-    ax2.plot(f1, label="noDROP")
-    ax2.plot(f2, label="DROP")
-    ax2.set_ylabel("L1 F")
-    ax2.set_xlabel("Epoch")
-    ax3.plot(a1, label="noDROP")
-    ax3.plot(a2, label="DROP")
-    ax3.set_ylabel("L1 Avg")
-    ax3.set_xlabel("Epoch")
-    ax1.legend()
-    ax2.legend()
-    ax3.legend()
-    plt.show()
-    plt.close()"""
-
-
-    #BAR REPRESENTATION
-    """ind = np.arange(4)
-    x_labels = ['NONE', 'MA', 'MA+EM', 'MA+EM+BW']
-    students = np.load(os.path.join(BAR_LIST,"list_bar_bad_example.npy"))
-    plt.bar(ind,students)
-    plt.xticks(ind,('NONE', 'MA', 'MA+EM', 'MA+EM+BW'))
-    plt.show()"""
-
-    """ECG_OUTPUTS_TEST = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                    "ECGOutputsTest")
-    LOSSES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Losses")
-
-
-    for filename in os.listdir(ECG_OUTPUTS_TEST): #present the fecg outputs
-        if "fecg" in filename:
-            num_of_f += 1
-            path = os.path.join(ECG_OUTPUTS_TEST, filename)
-            number_file = filename.index("g") + 1
-            end_path = filename[number_file:]
-            path_label = os.path.join(ECG_OUTPUTS_TEST,"label_f" + end_path)
-            real = np.load(path)
-            label = np.load(path_label)
-            correlation = check_correlation(real, label)
-            if(correlation < 0.70):
-                correlation_f += 1
-            fig, (ax1, ax2) = plt.subplots(2, 1)
-            ax1.plot(real)
-            ax1.set_ylabel("FECG")
-            ax2.plot(label)
-            ax2.set_ylabel("LABEL")
-            plt.show()
-            plt.close()
-
-
-        if "mecg" in filename:
-            num_of_m += 1
-            path = os.path.join(ECG_OUTPUTS_TEST, filename)
-            number_file = filename.index("g") + 1
-            end_path = filename[number_file:]
-            path_label = os.path.join(ECG_OUTPUTS_TEST, "label_m" + end_path)
-            real = np.load(path)
-            label = np.load(path_label)
-            correlation = check_correlation(real, label)
-            if (correlation < 0.70):
-                correlation_m += 1
-            fig, (ax1, ax2) = plt.subplots(2, 1)
-            ax1.plot(np.load(path))
-            ax1.set_ylabel("MECG")
-            ax2.plot(np.load(path_label))
-            ax2.set_ylabel("LABEL")
-            #plt.show()
-            plt.close()
-
-    print(correlation_f)
-    print(num_of_f)
-    print(correlation_m)
-    print(num_of_m)
-
-    path_losses = os.path.join(LOSSES, "TL1M.npy")
-    train_loss_m_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "TL1F.npy")
-    train_loss_f_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "TL1Avg.npy")
-    train_loss_average_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "VL1M.npy")
-    validation_loss_m_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "VL1F.npy")
-    validation_loss_f_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "VL1Avg.npy")
-    validation_loss_average_list = np.load(path_losses)
-
-    path_losses = os.path.join(LOSSES, "CorrF.npy")
-    correlation_f_list = np.load(path_losses)
-    path_losses = os.path.join(LOSSES, "CorrM.npy")
-    correlation_m_list = np.load(path_losses)
-
-    # plotting validation and training losses and saving them
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.plot(train_loss_m_list, label="training")
-    ax1.plot(validation_loss_m_list, label="validation")
-    ax1.set_ylabel("L1 M")
-    ax1.set_xlabel("Epoch")
-    ax2.plot(train_loss_f_list, label="training")
-    ax2.plot(validation_loss_f_list, label="validation")
-    ax2.set_ylabel("L1 F")
-    ax2.set_xlabel("Epoch")
-    ax3.plot(train_loss_average_list, label="training")
-    ax3.plot(validation_loss_average_list, label="validation")
-    ax3.set_ylabel("L1 Avg")
-    ax3.set_xlabel("Epoch")
-    ax1.legend()
-    ax2.legend()
-    ax3.legend()
-    plt.show()
-    plt.close()
-
-    fig, (ax1, ax2) = plt.subplots(2, 1)
-    ax1.plot(correlation_f_list)
-    ax1.set_ylabel("CorrF")
-    ax1.set_xlabel("Epoch")
-    ax2.plot(correlation_m_list)
-    ax2.set_ylabel("CorrM")
-    ax2.set_xlabel("Epoch")
-    plt.show()
-    plt.close()"""
--- model.py
+++ model.py
@@ -18,9 +18,9 @@ if not os.path.exists(network_save_folder_orig):
 
 delta = 3
 
-fecg_lamda = 1000
-cent_lamda = 1
-hinge_lamda = 1
+fecg_lamda = 10
+cent_lamda = 0.01
+hinge_lamda = 0.5
 
 mecg_weight = 1.
 fecg_weight = 1.
@@ -60,7 +60,6 @@ def train(resnet_model,
     total_loss_epoch = 0.
     total_loss_m = 0.
     total_loss_f = 0.
-    #total_loss_ecg = 0. #TODO add when real data
     total_loss_cent = 0.
     total_loss_hinge = 0.
 
@@ -75,7 +74,6 @@ def train(resnet_model,
                                      [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 0]]
 
-    # real_epoch = False #TODO add when real data
     for i, batch_features in enumerate(train_data_loader_sim):
         optimizer_model.zero_grad()
 
@@ -127,17 +125,13 @@ def train(resnet_model,
             path = os.path.join(ECG_OUTPUTS, "mecg" + str(i))
             np.save(path, outputs_m[0][0].cpu().detach().numpy())
 
-        # if not real_epoch: #TODO add when real data
         # COST(M,M^)
         train_loss_mecg = criterion(outputs_m, batch_for_m)
 
         # COST(F,F^)
         train_loss_fecg = criterion(outputs_f, batch_for_f)
-
-        # else: #TODO add when real data
-        #   outputs_m += outputs_f
-        #   train_loss_ecg = criterion(outputs_m, batch_for_model)
-
+        print(train_loss_mecg.item())
+        print(train_loss_fecg.item())
         flatten_m, flatten_f = torch.flatten(one_before_last_m, start_dim=1), torch.flatten(one_before_last_f,
                                                                                             start_dim=1)
         hinge_loss = criterion_hinge_loss(one_before_last_m, one_before_last_f, delta)
@@ -145,49 +139,37 @@ def train(resnet_model,
         labels_center_loss = Variable(torch.cat((torch.zeros(batch_size), torch.ones(batch_size))).cuda())
         loss_cent = criterion_cent(torch.cat((flatten_f, flatten_m), 0), labels_center_loss)
 
-        # if not real_epoch: #TODO add when real data
         total_loss = mecg_weight * train_loss_mecg + fecg_weight * fecg_lamda * train_loss_fecg
         if include_center_loss:
             total_loss += cent_weight * cent_lamda * loss_cent
         if include_hinge_loss:
             total_loss += hinge_weight * hinge_lamda * hinge_loss
-        # else: #TODO add when real data
-        #     total_loss = train_loss_ecg + cent_weight*cent_lamda*loss_cent + hinge_weight*hinge_lamda*hinge_loss #TODO: check lamda for ecg and change loss ecg
 
         total_loss.backward()
         optimizer_model.step()
 
-        # if not real_epoch: #TODO add when real data
         total_loss_m += mecg_weight * train_loss_mecg.item()
         total_loss_f += fecg_weight * fecg_lamda * train_loss_fecg.item()
-
-
-        # else: #TODO add when real data
-        #   total_loss_ecg += train_loss_ecg.item()
+        print(total_loss_m)
+        print(total_loss_f)
 
         total_loss_cent += cent_weight * cent_lamda * loss_cent.item()
         total_loss_hinge += hinge_weight * hinge_lamda * hinge_loss.item()
         total_loss_epoch += total_loss.item()
-        batch_features, batch_for_model, batch_for_m, batch_for_f, total_loss, outputs_m, one_before_last_m, \
-        outputs_f, one_before_last_f, train_loss_mecg, train_loss_fecg = None, None, None, None, None, None, None, \
-                                                                         None, None, None, None
 
-    # compute the epoch training loss
-    # if not real_epoch: #TODO add when real data
     total_loss_m = total_loss_m / (len(train_data_loader_sim.dataset))
     total_loss_f = total_loss_f / (len(train_data_loader_sim.dataset))
+    print(total_loss_m)
+    print(total_loss_f)
     train_loss_f_list.append(total_loss_f)
     train_loss_m_list.append(total_loss_m)
     train_loss_average_list.append((total_loss_m+total_loss_f)/2)
 
-    # else: #TODO add when real data
-    #    total_loss_ecg = total_loss_ecg / (len(train_data_loader_sim))
     total_loss_cent = total_loss_cent / (len(train_data_loader_sim.dataset))
     total_loss_hinge = total_loss_hinge / (len(train_data_loader_sim.dataset))
     total_loss_epoch = total_loss_epoch / (len(train_data_loader_sim.dataset))
-
+    print(len(train_data_loader_sim.dataset))
     # display the epoch training loss
-    # if not real_epoch: #TODO add when real data
     print("epoch S : {}/{}  total_loss = {:.8f}".format(epoch + 1, epochs, total_loss_epoch))
     if include_mecg_loss:
         print("loss_mecg = {:.8f} ".format(total_loss_m))
@@ -206,10 +188,6 @@ def train(resnet_model,
                                                             total_loss_cent,
                                                             total_loss_hinge,
                                                             total_loss_epoch))
-    # else: #TODO add when real data
-    #    print("epoch R : {}/{}, total_loss = {:.8f}, loss_ecg = {:.8f}, loss_cent = {:.8f}, loss_hinge = {:.8f}".format(
-    #            epoch + 1, epochs, total_loss_epoch, total_loss_ecg, total_loss_cent, total_loss_hinge))
-
 
 def val(val_data_loader_sim,
         resnet_model,
@@ -221,8 +199,9 @@ def val(val_data_loader_sim,
         validation_loss_average_list,
         validation_corr_m_list,
         validation_corr_f_list,
-        best_model_accuracy,
-        criterion_cent):
+        best_model_accuracy):
+    total_val_loss_m = 0
+    total_val_loss_f = 0
     val_loss_m = 0
     val_loss_f = 0
     val_corr_m = 0
@@ -232,14 +211,16 @@ def val(val_data_loader_sim,
             batch_for_model_val = Variable(batch_features[0].transpose(1, 2).float().cuda())
             batch_for_m_val = Variable(batch_features[1].transpose(1, 2).float().cuda())
             batch_for_f_val = Variable(batch_features[2].transpose(1, 2).float().cuda())
-            outputs_m_test, _, outputs_f_test, _ = resnet_model(batch_for_model_val)
-            val_loss_m += criterion(outputs_m_test, batch_for_m_val)
-            val_loss_f += (criterion(outputs_f_test, batch_for_f_val)* fecg_weight)
-            for j, elem in enumerate(outputs_m_test):
+            outputs_m_val, _, outputs_f_val, _ = resnet_model(batch_for_model_val)
+            val_loss_m = criterion(outputs_m_val, batch_for_m_val)
+            val_loss_f = criterion(outputs_f_val, batch_for_f_val)
+            print(val_loss_m.item())
+            print(val_loss_f.item())
+            for j, elem in enumerate(outputs_m_val):
                 val_corr_m += \
-                np.corrcoef(outputs_m_test.cpu().detach().numpy()[j], batch_for_m_val.cpu().detach().numpy()[j])[0][1]
+                np.corrcoef(outputs_m_val.cpu().detach().numpy()[j], batch_for_m_val.cpu().detach().numpy()[j])[0][1]
                 val_corr_f += \
-                np.corrcoef(outputs_f_test.cpu().detach().numpy()[j], batch_for_f_val.cpu().detach().numpy()[j])[0][1]
+                np.corrcoef(outputs_f_val.cpu().detach().numpy()[j], batch_for_f_val.cpu().detach().numpy()[j])[0][1]
 
             if epoch + 1 == epochs:
                 if not os.path.exists(ECG_OUTPUTS_VAL):
@@ -254,25 +235,31 @@ def val(val_data_loader_sim,
                 np.save(path, outputs_f_test[0][0].cpu().detach().numpy() )
                 path = os.path.join(ECG_OUTPUTS_VAL, "mecg" + str(i))
                 np.save(path, outputs_m_test[0][0].cpu().detach().numpy())
+            total_val_loss_m += val_loss_m.item() * mecg_weight
+            total_val_loss_f += val_loss_f.item() * fecg_weight * fecg_lamda
+            print(total_val_loss_m)
+            print(total_val_loss_f)
 
-    val_loss_m /= len(val_data_loader_sim.dataset)
-    val_loss_f /= len(val_data_loader_sim.dataset)
+    total_val_loss_m /= len(val_data_loader_sim.dataset)
+    total_val_loss_f /= len(val_data_loader_sim.dataset)
+    print(total_val_loss_m)
+    print(total_val_loss_f)
     val_corr_m /= len(val_data_loader_sim.dataset)
     val_corr_f /= len(val_data_loader_sim.dataset)
     val_corr_average = (val_corr_m + val_corr_f) / 2
-    val_loss_average = (val_loss_m + val_loss_f) / 2
+    val_loss_average = (total_val_loss_m + total_val_loss_f) / 2
 
     # saving validation losses
-    validation_loss_m_list.append(val_loss_m.cpu().detach())
-    validation_loss_f_list.append(val_loss_f.cpu().detach())
-    validation_loss_average_list.append(val_loss_average.cpu().detach())
+    validation_loss_m_list.append(total_val_loss_m)
+    validation_loss_f_list.append(total_val_loss_f)
+    validation_loss_average_list.append(val_loss_average)
     validation_corr_m_list.append(val_corr_m)
     validation_corr_f_list.append(val_corr_f)
     if epoch + 1 == epochs:
         with open("val_loss_last_epoch.txt", 'w') as f:
             f.write("L1 M = {:.4f},L1 F= {:.4f},LAvg = {:.4f},"
-                    "CorrM = {:.4f},CorrF = {:.4f}, CorrAvg = {:.4f}\n".format(val_loss_m,
-                                                            val_loss_f,
+                    "CorrM = {:.4f},CorrF = {:.4f}, CorrAvg = {:.4f}\n".format(total_val_loss_m,
+                                                            total_val_loss_f,
                                                             val_loss_average,
                                                             val_corr_m,
                                                             val_corr_f,
@@ -284,14 +271,13 @@ def val(val_data_loader_sim,
     if (val_corr_average > best_model_accuracy):
         best_model_accuracy = val_corr_average
         torch.save(resnet_model.state_dict(), str(network_save_folder_orig + network_file_name_best))
-        torch.save(criterion_cent.state_dict(), str(network_save_folder_orig + '/criterion_cent'))
         print("saving best model")
         with open("best_model_epoch.txt", 'w') as f:
             f.write(str(epoch))
 
     print(
         'Validation: Average loss M: {:.4f}, Average Loss F: {:.4f}, Average Loss M+F: {:.4f}, Correlation M: {:.4f},Correlation F: {:.4f},Correlation Average: {:.4f})\n'.format(
-            val_loss_m, val_loss_f, val_loss_average, val_corr_m, val_corr_f, val_corr_average))
+            total_val_loss_m, total_val_loss_f, val_loss_average, val_corr_m, val_corr_f, val_corr_average))
     return best_model_accuracy,val_loss_average
 
 
