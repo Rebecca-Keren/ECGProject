d49f8873563311db947d4204b9f179165c1eaef1
rebecca <rebecca_raccah@hotmail.com>
2021-06-14 17:07:05 +0300

    normalization

--- NormalizingSignals.py
+++ NormalizingSignals.py
@@ -6,7 +6,7 @@ import os
 import scipy.io as sio
 from SignalPreprocessing.data_agumentation_function import *
 
-REAL_SIGNALS = "NewReal"
+REAL_SIGNALS = "SignalsReal"
 NEW_SIGNALS = "NormalizedReal"
 
 if not os.path.exists(NEW_SIGNALS):
@@ -23,5 +23,9 @@ if __name__ == '__main__':
         abs_signal = [abs(signal[i]) for i in range(len(signal))]
         max_signal = max(abs_signal)
         signal_new = [sample / max_signal for sample in signal]
-        resampled_signal = increase_sampling_rate(signal_new,0.4)
-        sio.savemat(os.path.join(new_dir,filename), {'data': resampled_signal})
+        resampled_signal = increase_sampling_rate(signal_new,0.25)
+        number_of_window = int(len(resampled_signal) / 1024)
+        window_size = 1024
+        for k in range(number_of_window):
+            record = np.array(resampled_signal[k * window_size:(k + 1) * window_size])
+            sio.savemat(os.path.join(new_dir, filename + str(k)), {'data': record})
