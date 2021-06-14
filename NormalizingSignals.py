70a0a6a7a35785cf858fd700f0c29902d62b0bf4
rebecca <rebecca_raccah@hotmail.com>
2021-06-14 16:26:34 +0300

    normalization

---
+++ NormalizingSignals.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import wfdb
+from scipy.io import loadmat
+import os
+import scipy.io as sio
+
+REAL_SIGNALS = "NewReal"
+NEW_SIGNALS = "NormalizedReal"
+
+if not os.path.exists(NEW_SIGNALS):
+    os.mkdir(NEW_SIGNALS)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+real_dir = os.path.join(dir_path,REAL_SIGNALS)
+new_dir = os.path.join(dir_path,NEW_SIGNALS)
+
+if __name__ == '__main__':
+    # Merging Data,dividing into windows and saving
+    for filename in os.listdir(real_dir):
+        signal = loadmat(os.path.join(real_dir, filename))['data'][0]
+        abs_signal = [abs(signal[i]) for i in range(len(signal))]
+        max_signal = max(abs_signal)
+        signal_new = [sample / max_signal for sample in signal]
+        sio.savemat(os.path.join(new_dir,filename), {'data': signal_new})
